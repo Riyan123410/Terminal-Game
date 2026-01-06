f81ea677941f5c7590b2e83747006a475cfcddae
Quoric <thelegendarymr.fish@gmail.com>
2026-01-06 18:54:18 -0500

    Added enemy intentions file + clear terminal helper

---
+++ terminalGame/enemyIntentions.py
@@ -0,0 +1,11 @@
+intentionsList = {"goose" : 
+                  {"turn1" : {"attackTimes" : 2, 
+                              1 : {"description" : "deal 2d4 damage", "effect" : "damagePlayer(1,diceRoll(2,4))"}, 
+                              2 : {"description" : "Discard 1 card from your hand", "effect" : "discardCardRand(1)"}
+                              }, 
+                    "turn2" : {"attackTimes" : 2, 
+                                1 : {"description" : "deal 2d4 damage", "effect" : "damagePlayer(1,diceRoll(2,4))"}, 
+                                2 : {"description" : "Discard 1 card from your hand", "effect" : "discardCardRand(1)"}
+                                }
+                    }
+                 }
--- terminalGame/helperFuncs.py
+++ terminalGame/helperFuncs.py
@@ -1,3 +1,5 @@
+import os
+
 def clamp(num, minNum, maxNum):
     if num < minNum:
         return minNum
@@ -8,3 +10,13 @@ def clamp(num, minNum, maxNum):
 # change and clamp
 def changeWithClamp(num, change, minNum, maxNum):
     return clamp(num + change, minNum, maxNum)
+
+# clear the terminal
+def clearTerminal():
+    # Check the operating system name
+    if os.name == 'nt':
+        # Command for Windows
+        os.system('cls')
+    else:
+        # Command for Linux and macOS
+        os.system('clear')
