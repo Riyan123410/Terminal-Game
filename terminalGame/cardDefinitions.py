379e5746319c0050e91d072a31cc8c03471fb1e8
Quoric <thelegendarymr.fish@gmail.com>
2026-01-07 12:59:00 -0500

    removed more clutter

--- terminalGame/cardDefinitions.py
+++ terminalGame/cardDefinitions.py
@@ -1,5 +1,1 @@
-import random
-import helperFuncs
-import time
-
 cardDef = {"end" : ["passes turn", "print('how are you seeing this')", 0], "define" : ["defines",  "defineCard()", 0], "strike" : ["deals 4 damage, 1 target", "damageEnemy(1,4)", 1], "block" : ["gain 3 block", "gainBlock(1,3)", 1], "clean sweep" : ["discard 2 cards, then gain 2 cost", "discardGain(2)", 0]}
