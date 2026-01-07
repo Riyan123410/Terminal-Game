3140e22ad79fe32510a33e0188d7eef0c19e7141
Cookie <r1yankhan1135@gmail.com>
2026-01-06 20:54:45 -0500

    added ascii helpers

--- terminalGame/asciiArt/asciiHelpers.py
+++ terminalGame/asciiArt/asciiHelpers.py
@@ -1,23 +1,23 @@
 # add a character to the string
 def addRowChars(string, legnth, i):
-    rowChars = ""
-    for j in range(legnth):
-        try:
-            rowChars += string[i * legnth + j]
-        except:
-            return "OUT OF RANGE - this shouldnt be seen"
-    return rowChars
+   rowChars = ""
+   for j in range(legnth):
+       try:
+           rowChars += string[i * legnth + j]
+       except:
+           return "OUT OF RANGE - this shouldnt be seen"
+   return rowChars
 
 
 def combineStrings(string1, len1, string2, len2, height):
-    # remove all end lines and create a new string
-    string1 = string1.replace("\n", "")
-    string2 = string2.replace("\n", "")
-    combinedString = ""
-    # loop through the height of the strings
-    for i in range(height):
-        # loop through the legnth of each adding to the new string
-        combinedString += addRowChars(string1, len1, i)
-        combinedString += addRowChars(string2, len2, i)
-        combinedString += "\n"
-    return combinedString
+   # remove all end lines and create a new string
+   string1 = string1.replace("\n", "")
+   string2 = string2.replace("\n", "")
+   combinedString = ""
+   # loop through the height of the strings
+   for i in range(height):
+       # loop through the legnth of each adding to the new string
+       combinedString += addRowChars(string1, len1, i)
+       combinedString += addRowChars(string2, len2, i)
+       combinedString += "\n"
+   return combinedString
