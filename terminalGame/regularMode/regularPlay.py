69316dc0e5a00b124566d2459554dc4937927944
Cookie <r1yankhan1135@gmail.com>
2026-01-09 12:28:10 -0500

    reg player

---
+++ terminalGame/regularMode/regularPlay.py
@@ -0,0 +1,20 @@
+import player
+
+playerHealth = 50
+costMax = 4
+cost = 0
+playerBlock = 0
+difficulty = 0.5
+combatDifficulty = 0
+costGain = 3
+startingDraw = 5
+turnNumber = 0
+visibleIntentions = {}
+handMax = 14
+discard = []
+hand = []
+deck = ["strike", "clean sweep", "strike", "strike", "strike", "strike", "strike", "strike", "strike", "block", "block", "block", "crossbow", "well prepared"]
+enemies = dict({})
+
+def main():
+    return "quit"
